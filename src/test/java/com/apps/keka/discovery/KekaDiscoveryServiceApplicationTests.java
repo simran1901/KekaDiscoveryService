9320e4fa8c41c5c6912a5ff7473a0ae533ad5d28
Simran Makhijani <simranmakhijani55@gmail.com>
2022-08-18 14:32:55 +0530

    added project

---
+++ src/test/java/com/apps/keka/discovery/KekaDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apps.keka.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KekaDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
